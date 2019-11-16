c13c28354b20ce0bcaf36e29759158d10e9542d7
Steve Harrison <steve@harricoder.com>
2019-11-16 23:12:26 +0000

    scripts: improved update script

--- osx_update.sh
+++ osx_update.sh
@@ -1,3 +1,12 @@
 # TODO: Not sure if I want this script?
-pip install --upgrade setuptools
-pip install --upgrade pip
+pip3 install --upgrade setuptools
+pip3 install --upgrade pip
+pip3 install --upgrade virtualenv
+
+# Update brew and brew casks
+brew upgrade
+brew cask upgrade
+brew cleanup
+
+# Upgrade MAS stuff
+mas upgrade
