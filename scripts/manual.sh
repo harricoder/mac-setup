50fb4a1a2da59953512e97a0281d3258db4032ad
Steve Harrison <steve@harricoder.com>
2020-07-22 23:35:31 +0100

    scripts: included a few more manual reminders

--- scripts/manual.sh
+++ scripts/manual.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #
 # Manual things:
+#
 # Some things just can't be automated, make a note of any 'things' that need
 # setting up manually - that is to say they can't currently be automated, or
-# are just too awkward to do so at the moment.
+# are just too awkward to do so at the moment
 function man_prompt {
   echo
   echo -e "${YELLOW}$1${NC}"
@@ -12,10 +13,12 @@ function man_prompt {
 }
 
 man_prompt "Plug in iphone and turn off auto sync in itunes and photos"
+man_prompt "Allow Logi Options access in Security & Privacy security settings"
 man_prompt "Copy across iTerm2 User profile"
+man_prompt "Adjust monitor arrangement in settings"
 man_prompt "Update: Profile Image, Desktop Background and Screen saver"
 man_prompt "Install Elgato Stream Deck Profile (from cloud backup)"
 man_prompt "Copy across or create new ssh key -> add to bitbucket, github, etc"
 
 echo
-echo -e "${GREEN_TICK} Manual actions are all done.${NC}"
+echo -e "${GREEN_TICK} Manual actions are all complete.${NC}"
