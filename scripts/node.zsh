94d396b56ba26cc852a7720f1cb903cf41bda9c3
Steve Harrison <steve@harricoder.com>
2021-02-01 22:52:46 +0000

    node: update nvm script

--- scripts/node.zsh
+++ scripts/node.zsh
@@ -2,15 +2,24 @@
 #
 # NVM / Node (requires .zshrc)
 source ~/.zshrc
+
+wanted_nvm_version="0.37.2"
+
 if [ -f "~/.nvm/nvm.sh" ]; then
     source ~/.nvm/nvm.sh
 fi
 
-if ! [ "$(command -v nvm)" ]; then
-  echo "installing nvm..."
-  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+if ! [ "$(command -v nvm)" ] || [ "$(nvm --version 2>&1)" != "$wanted_nvm_version" ]; then
+  if ! [ "$(command -v nvm)" ]; then
+    echo "installing nvm..."
+  else
+    echo "updating nvm..."
+  fi
+
+  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v${wanted_nvm_version}/install.sh | bash
   source ~/.nvm/nvm.sh
 fi
+
 echo "nvm is installed: $(nvm --version)"
 
 if ! [ "$(command -v node)" ]; then
@@ -18,4 +27,5 @@ if ! [ "$(command -v node)" ]; then
   source ~/.nvm/nvm.sh
   nvm install node --latest-npm
 fi
+
 echo -e "${GREEN_TICK} node successfully installed: $(node --version) with npm: $(npm --version)${NC}"
